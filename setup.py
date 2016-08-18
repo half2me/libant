c9db4d2fab45020965589cbeef6525d615a1221c
Benjamin Tamási <h@lfto.me>
2016-08-18 20:44:30 +0200

    Change to MIT licence

---
+++ setup.py
@@ -0,0 +1,13 @@
+#!/usr/bin/env python
+
+from distutils.core import setup
+
+setup(name='LibAnt',
+      version='0.1',
+      description='Python Ant+ Lib',
+      author='Benjamin Tamasi',
+      author_email='h@lfto.me',
+      url='https://github.com/half2me/libAnt',
+      licence='MIT',
+      packages=['libAnt'],
+      )
