a63176abae4971827aa3ca6bbd3b3551d1178cfe
Benjamin Tamási <h@lfto.me>
2016-08-24 11:49:34 +0200

    Driver

--- libAnt/driver.py
+++ libAnt/driver.py
@@ -1,5 +1,6 @@
 from abc import abstractmethod
 from threading import Lock
+from serial import Serial, SerialException, SerialTimeoutException
 
 import usb
 
@@ -32,19 +33,23 @@ class Driver:
 class SerialDriver(Driver):
     def __init__(self, device, baudRate=115200):
         super().__init__()
-        self.device = device
-        self.baudRate = baudRate
+        self._device = device
+        self._baudRate = baudRate
+        self._serial = None
+
+    def _isOpen(self):
+        return self._serial is None
+
+    def _open(self):
+        try:
+            self._dev = Serial(self._device, self._baudRate)
+        except SerialException as e:
+            raise e  # TODO: Make DriverException
 
     def _close(self):
         pass
 
-    def _open(self):
-        pass
-
     def _write(self, data):
-        pass
-
-    def _isOpen(self):
         pass
 
     def _read(self, count):
@@ -64,14 +69,14 @@ class USB2Driver(Driver):
     def _open(self):
         self._dev = usb.core.find(idVendor=self._idVendor, idProduct=self._idProduct)
         if self._dev is None:
-            pass  # TODO: Need exception here
+            pass  # # TODO: Make DriverException
 
         # take care of bugs with the kernel driver
         if self._dev.is_kernel_driver_active(0):
             try:
                 self._dev.detach_kernel_driver(0)
             except usb.USBError as e:
-                pass  # TODO: Exception here couldn't detach kernel driver
+                pass  # TODO: Make DriverException
 
         self._dev.set_configuration()
         cfg = self._dev.get_active_configuration()
@@ -83,8 +88,8 @@ class USB2Driver(Driver):
 
         self._epOut = usb.util.find_descriptor(interface, custom_match=lambda e: usb.util.endpoint_direction(
             e.bEndpointAddress) == usb.ENDPOINT_OUT)
-        assert self._epOut is not None  # TODO: change to exception
+        assert self._epOut is not None  # TODO: Make DriverException
 
         self._ep_in = usb.util.find_descriptor(interface, custom_match=lambda e: usb.util.endpoint_direction(
             e.bEndpointAddress) == usb.ENDPOINT_IN)
-        assert self._ep_in is not None  # TODO: change to exception
+        assert self._ep_in is not None  # TODO: Make DriverException
