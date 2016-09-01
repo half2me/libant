a6d1a2daea90545ed05a296c670b08579a4cce67
Tapasztó Lejla <Crabhill4>
2016-09-01 20:21:02 +0200

    thread

--- libAnt/node.py
+++ libAnt/node.py
@@ -1,7 +1,37 @@
+import threading
+
+
+class Network:
+    def __init__(self, key=b'\x00' * 8, name=None):
+        self.key = key
+        self.name = name
+        self.number = 0
+
+    def __str__(self):
+        return self.name + self.key
+
+
+class Pump(threading.Thread):
+    def __init__(self):
+        super().__init__()
+        self._stop = threading.Event()
+
+    def stop(self):
+        self._stop.set()
+
+    def stopped(self):
+        return self._stop.isSet()
+
+    def run(self):
+        pass
+
+
 class Node:
     def __init__(self, driver, name=None):
         self._driver = driver
         self._name = name
+        self.running = False
+        self.pump = Pump()
 
     def __enter__(self):
         return self
@@ -10,10 +40,15 @@ class Node:
         self.stop()
 
     def start(self):
-        pass
+        if not self.running:
+            self.pump.start()
+            self.running = True
 
     def stop(self):
-        pass
+        if self.running:
+            self.pump.stop()
+            self.pump.join()
+            self.running = False
 
     def reset(self):
         pass
