4aaa2e1b9076b59815ef3d62a2159ccfb5e310cc
Tapasztó Lejla <Crabhill4>
2016-09-05 12:01:42 +0200

    writing to device is not working yet

--- test.py
+++ test.py
@@ -2,5 +2,5 @@ from libAnt.driver import USBDriver
 
 driver = USBDriver(vid=0x0FCF, pid=0x1008)
 with driver as d:
-    buf = d.read(1)
-    print(buf)
+    d.write(bytearray(0x00))
+    print("yolo")
