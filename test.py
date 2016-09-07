1e04fdaaf69a16fe39b25741b84e756a2951e1cb
Tamási Benjamin <halftome@yahoo.com>
2016-09-07 20:42:18 +0200

    Network key working

--- test.py
+++ test.py
@@ -1,13 +1,16 @@
 #!/usr/bin/env python3
-from libAnt.constants import ANTPLUS_NETWORK_KEY, MESSAGE_NETWORK_KEY
+from libAnt.constants import ANTPLUS_NETWORK_KEY, MESSAGE_NETWORK_KEY, MESSAGE_SYSTEM_RESET
 from libAnt.driver import USBDriver
 from libAnt.message import Message
 
 driver = USBDriver(vid=0x0FCF, pid=0x1008)
 with driver as d:
-    msg = Message(MESSAGE_NETWORK_KEY, ANTPLUS_NETWORK_KEY)
+    b = bytearray()
+    b.append(0)
+    b.extend(ANTPLUS_NETWORK_KEY)
+    msg = Message(MESSAGE_NETWORK_KEY, b)
     print(msg)
-    d.write(msg)  # system
+    d.write(msg)
     ret = d.read()
     print(ret)
 
