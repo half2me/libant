6a64985a9a6581e21c2d9f8a57e5741eb0689ed0
Tapasztó Lejla <Crabhill4>
2016-09-11 11:45:35 +0200

    Examples

---
+++ demos/01-simple_rx_scan.py
@@ -0,0 +1,19 @@
+#!/usr/bin/env python3
+from time import sleep
+
+from libAnt.driver import USBDriver
+from libAnt.node import Node
+
+
+def callback(msg):
+    print(msg)
+
+
+def eCallback(e):
+    print(e)
+
+
+with Node(USBDriver(vid=0x0FCF, pid=0x1008), 'MyNode') as n:
+    n.enableRxScanMode()
+    n.start(callback, eCallback)
+    sleep(30)  # Listen for 30sec
