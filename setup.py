95c1a7be647e08ae6999dcdeb86b0e3f7ecdb28e
Tapasztó Lejla <Crabhill4>
2016-09-11 11:58:32 +0200

    0.1.1

--- setup.py
+++ setup.py
@@ -3,11 +3,11 @@
 from distutils.core import setup
 
 setup(name='LibAnt',
-      version='0.1',
+      version='0.1.1',
       description='Python Ant+ Lib',
       author='Benjamin Tamasi',
       author_email='h@lfto.me',
       url='https://github.com/half2me/libAnt',
       licence='MIT',
-      packages=['libAnt'],
+      packages=['libAnt', 'libAnt.profiles'],
       )
