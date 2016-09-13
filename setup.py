74ac5e1e1ae0a00bb00f242bcaa0f69b444bf449
Tamási Benjamin <halftome@yahoo.com>
2016-09-13 09:32:25 +0200

    0.1.2

--- setup.py
+++ setup.py
@@ -2,12 +2,16 @@
 
 from distutils.core import setup
 
+with open('requirements.txt') as f:
+    requirements = f.read().splitlines()
+
 setup(name='LibAnt',
-      version='0.1.1',
+      version='0.1.2',
       description='Python Ant+ Lib',
       author='Benjamin Tamasi',
       author_email='h@lfto.me',
       url='https://github.com/half2me/libAnt',
+      install_requires=requirements,
       licence='MIT',
       packages=['libAnt', 'libAnt.profiles'],
       )
