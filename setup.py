022fd3dd2e63bc82b2d703834275bf11b37cf82f
Tamási Benjamin <halftome@yahoo.com>
2016-09-13 10:42:13 +0200

    Update Readme

--- setup.py
+++ setup.py
@@ -5,13 +5,15 @@ from distutils.core import setup
 with open('requirements.txt') as f:
     requirements = f.read().splitlines()
 
-setup(name='LibAnt',
-      version='0.1.2',
-      description='Python Ant+ Lib',
-      author='Benjamin Tamasi',
-      author_email='h@lfto.me',
-      url='https://github.com/half2me/libAnt',
-      install_requires=requirements,
-      licence='MIT',
-      packages=['libAnt', 'libAnt.profiles'],
-      )
+setup(
+    name='LibAnt',
+    packages=['libAnt', 'libAnt.profiles'],
+    version='0.1.2',
+    description='Python Ant+ Lib',
+    author='Benjamin Tamasi',
+    author_email='h@lfto.me',
+    url='https://github.com/half2me/libAnt',
+    download_url='https://github.com/half2me/libAnt/tarball/0.1.2',
+    keywords = ['ant', 'antplus', 'ant+', 'antfs', 'thisisant'],
+    install_requires=requirements,
+)
