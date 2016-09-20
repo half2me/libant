04a64330e36eaa9114c80ea7d6dd8f3e9b834743
Tapasztó Lejla <Crabhill4>
2016-09-20 14:13:26 +0200

    0.1.3

--- setup.py
+++ setup.py
@@ -10,7 +10,7 @@ setup(
     author='Benjamin Tamasi',
     author_email='h@lfto.me',
     url='https://github.com/half2me/libAnt',
-    download_url='https://github.com/half2me/libAnt/tarball/0.1.2',
+    download_url='https://github.com/half2me/libAnt/tarball/0.1.3',
     keywords = ['ant', 'antplus', 'ant+', 'antfs', 'thisisant'],
     install_requires=['pyusb>=1.0.0', 'pyserial>=3.1.1'],
 )
