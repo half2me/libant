85ec75e3d245c5ce530913cdd724099443ab72ef
glevente <gergely.levente0@gmail.com>
2016-10-06 13:04:48 +0200

    fix Logger

--- libAnt/driver.py
+++ libAnt/driver.py
@@ -17,6 +17,71 @@ from libAnt.message import Message, SystemResetMessage
 class DriverException(Exception):
     pass
 
+class Logger:
+    def __init__(self, logFile: str):
+        self._logFile = logFile
+        self._log = None
+
+    def __enter__(self):
+        self.open()
+        return self
+
+    def __exit__(self, exc_type, exc_val, exc_tb):
+        self.close()
+
+    def open(self):
+        if self._log is not None:
+            self.close()
+        self._log = open(self._logFile, 'wb')
+        self.onOpen()
+
+    def close(self):
+        if self._log is not None:
+            self.beforeClose()
+            self._log.close()
+            self.afterClose()
+
+    def log(self, data: bytes):
+        self._log.write(self.encodeData(data))
+
+    def onOpen(self):
+        pass
+
+    def beforeClose(self):
+        pass
+
+    def afterClose(self):
+        pass
+
+    def encodeData(self, data):
+        return data
+
+class PcapLogger(Logger):
+    def onOpen(self):
+        # write pcap global header
+        magic_number = b'\xD4\xC3\xB2\xA1'
+        version_major = 2
+        version_minor = 4
+        thiszone = b'\x00\x00\x00\x00'
+        sigfigs = b'\x00\x00\x00\x00'
+        snaplen = b'\xFF\x00\x00\x00'
+        network = b'\x01\x00\x00\x00'
+        pcap_global_header = Struct('<4shh4s4s4s4s')
+        self._log.write(
+            pcap_global_header.pack(magic_number, version_major, version_minor, thiszone, sigfigs,
+                                    snaplen, network))
+
+    def encodeData(self, data):
+        timestamp = time.time()
+        frac, whole = math.modf(timestamp)
+
+        ts_sec = int(whole).to_bytes(4, byteorder='little')
+        ts_usec = int(frac * 1000 * 1000).to_bytes(4, byteorder='little')
+        incl_len = len(data)
+        orig_len = incl_len
+
+        pcap_packet_header = Struct('<4s4sll').pack(ts_sec, ts_usec, incl_len, orig_len)
+        return pcap_packet_header + data
 
 class Driver:
     """
@@ -79,23 +144,24 @@ class Driver:
                 chk = self._read(1, timeout=timeout)[0]
                 msg = Message(type, data)
 
-                if self._log:
+                if self._logger:
                     logMsg = bytearray([sync, length, type])
                     logMsg.extend(data)
                     logMsg.append(chk)
-                    timestamp = time.time() - self._openTime
-                    frac, whole = math.modf(timestamp)
+                    # timestamp = time.time() - self._openTime
+                    # frac, whole = math.modf(timestamp)
+                    #
+                    # ts_sec = int(whole).to_bytes(4, byteorder='little')
+                    # ts_usec = int(frac * 1000 * 1000).to_bytes(4, byteorder='little')
+                    # incl_len = len(logMsg)
+                    # orig_len = incl_len
 
-                    ts_sec = int(whole).to_bytes(4, byteorder='little')
-                    ts_usec = int(frac * 1000 * 1000).to_bytes(4, byteorder='little')
-                    incl_len = len(logMsg)
-                    orig_len = incl_len
+                    # pcap_packet_header = Struct('<4s4sll')
 
-                    pcap_packet_header = Struct('<4s4sll')
-
-                    with open(self._log, 'ab') as log:
-                        log.write(pcap_packet_header.pack(ts_sec, ts_usec, incl_len, orig_len))
-                        log.write(logMsg)
+                    self._logger.log(bytes(logMsg))
+                    # with open(self._log, 'ab') as log:
+                    #     log.write(pcap_packet_header.pack(ts_sec, ts_usec, incl_len, orig_len))
+                    #     log.write(logMsg)
 
                 if msg.checksum() == chk:
                     return msg
@@ -404,62 +470,3 @@ class PcapDriver(Driver):
 
     def _write(self, data: bytes) -> None:
         pass
-
-
-class Logger:
-    def __init__(self, logFile: str):
-        self._logFile = logFile
-        self._log = None
-
-    def __enter__(self):
-        self.open()
-        return self
-
-    def __exit__(self, exc_type, exc_val, exc_tb):
-        self.close()
-
-    def open(self):
-        if self._log is not None:
-            self.close()
-        self._log = open(self._logFile, 'wb')
-        self.onOpen()
-
-    def close(self):
-        if self._log is not None:
-            self.beforeClose()
-            self._log.close()
-            self.afterClose()
-
-    def log(self, data: bytes):
-        self._log.write(self.encodeData(data))
-
-    def onOpen(self):
-        pass
-
-    def beforeClose(self):
-        pass
-
-    def afterClose(self):
-        pass
-
-    def encodeData(self, data):
-        return data
-
-
-class PcapLogger(Logger):
-    def onOpen(self):
-        # write pcap global header
-        magic_number = b'\xD4\xC3\xB2\xA1'
-        version_major = 2
-        version_minor = 4
-        thiszone = b'\x00\x00\x00\x00'
-        sigfigs = b'\x00\x00\x00\x00'
-        snaplen = b'\xFF\x00\x00\x00'
-        network = b'\x01\x00\x00\x00'
-        pcap_global_header = Struct('<4shh4s4s4s4s')
-        self._log.write(
-            pcap_global_header.pack(magic_number, version_major, version_minor, thiszone, sigfigs,
-                                    snaplen, network))
-
-    def encodeData(self, data):
-        return data  # pack data into pcap packets here
