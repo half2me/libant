41c6429d313e034b37438dc1dd39269d7b5f5c0a
glevente <gergely.levente0@gmail.com>
2017-02-02 14:19:21 +0100

    0.2.0

--- setup.py
+++ setup.py
@@ -5,9 +5,12 @@ from setuptools import setup
 setup(
     name='LibAnt',
     packages=['libAnt', 'libAnt.profiles', 'libAnt.drivers', 'libAnt.loggers'],
-    version='0.1.8',
+    version='0.2.0',
     description='Python Ant+ Lib',
     author='Benjamin Tamasi',
     author_email='h@lfto.me',
+    url='https://github.com/half2me/libAnt',
+    download_url='https://github.com/half2me/libAnt/tarball/0.1.3',
+    keywords = ['ant', 'antplus', 'ant+', 'antfs', 'thisisant'],
     install_requires=['pyusb>=1.0.0', 'pyserial>=3.1.1'],
 )
