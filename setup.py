75cdefc820b7fe3ae5c63e5ad26f29c904f42a67
glevente <gergely.levente0@gmail.com>
2017-02-02 14:43:45 +0100

    0.2.4

--- setup.py
+++ setup.py
@@ -5,7 +5,7 @@ from setuptools import setup
 setup(
     name='LibAnt',
     packages=['libAnt', 'libAnt.profiles', 'libAnt.drivers', 'libAnt.loggers'],
-    version='0.2.3',
+    version='0.2.4',
     description='Python Ant+ Lib',
     author='Benjamin Tamasi',
     author_email='h@lfto.me',
