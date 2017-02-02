fcd4ea613a58c26592e0633a9dbd0cefa105b0a4
glevente <gergely.levente0@gmail.com>
2017-02-02 14:08:57 +0100

    0.1.7

--- setup.py
+++ setup.py
@@ -5,10 +5,10 @@ from setuptools import setup
 setup(
     name='LibAnt',
     packages=['libAnt', 'libAnt.profiles', 'libAnt.drivers', 'libAnt.loggers'],
-    version='0.1.6',
+    version='0.1.7',
     description='Python Ant+ Lib',
-    author='Benjamin Tamasi',
-    author_email='h@lfto.me',
+    author='glevente',
+    author_email='levente.gergely@ccsys.eu',
     url='https://github.com/half2me/libAnt',
     download_url='https://github.com/half2me/libAnt/tarball/0.1.3',
     keywords = ['ant', 'antplus', 'ant+', 'antfs', 'thisisant'],
