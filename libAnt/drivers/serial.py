39d126d942d93c9f63c70ae2a49024768eab047c
Doug Slater <doug@sltr.us>
2020-03-06 04:28:48 +0000

    Workaround pyserial 3.1 issue https://github.com/pyserial/pyserial/issues/139

--- libAnt/drivers/serial.py
+++ libAnt/drivers/serial.py
@@ -29,7 +29,7 @@ class SerialDriver(Driver):
 
     def _open(self) -> None:
         try:
-            self._serial = Serial(port=self._device, baudrate=self._baudRate)
+            self._serial = Serial(port=self._device, baudrate=self._baudRate, dsrdtr=True, rtscts=True)
         except SerialException as e:
             raise DriverException(str(e))
 
